cf71d0f019710f237000c3d098541ac2cca81120
shw16 <shw16@DESKTOP-HV8IUM9>
2020-03-18 21:48:01 +0900

    test

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,5 +1,5 @@
 
-public class HelloWorld{
+public class MenuManager{
 	public static void main(String[]args) {
 		System.out.println("*** Youtuber Management System Menu ***");
 		System.out.println(" 1. Add Youtuber");
