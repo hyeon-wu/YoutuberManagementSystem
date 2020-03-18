e132464441b374164bb9b8b913945d704b19ab51
shw16 <shw16@DESKTOP-HV8IUM9>
2020-03-18 21:45:37 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld{
+	public static void main(String[]args) {
+		System.out.println("*** Youtuber Management System Menu ***");
+		System.out.println(" 1. Add Youtuber");
+		System.out.println(" 2. Delete Youtuber");
+		System.out.println(" 3. Edit Youtuber");
+		System.out.println(" 4. View Youtuber");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5:");
+	}
+}
