cd62eab9c573b01ad0f2dedd780d17c8c0d284da
shw16 <shw16@DESKTOP-HV8IUM9>
2020-03-28 14:26:46 +0900

    edit menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -40,70 +40,25 @@ public class MenuManager{
 		System.out.print("Kind of Youtuber:");
 		String youtuberEmail=input.next();
 		System.out.print("Youtuber Link:");
-		String youtuberphone=input.next();
+		String youtuberlink=input.next();
 	}
-
 	public static void deleteYoutuber() {
 		Scanner input=new Scanner(System.in);
-		System.out.print("Youtuber name:");
+		System.out.print("Youtuber ID:");
 		String youtubername=input.next();
-		System.out.print("Delete "+youtubername+"?\n"+"Y or N 입력: ");
-		String answer=input.next();
-		switch(answer) {
-		case("Y"):
-			System.out.println(youtubername+"유튜버가 삭제되었습니다!");
-		case("N"):
-			break;
-		}
 	}
 	public static void editYoutuber() {
 		Scanner input=new Scanner(System.in);
-		System.out.print("Youtuber name:");
+		System.out.print("Youtuber ID:");
 		String youtubername=input.next();
-		System.out.print("편집하고자 하는 유튜버가 "+youtubername+"이 맞습니까?\nY or N 입력: ");
-		String answer=input.next();
-		switch(answer){
-		case("Y"):
-			System.out.println(youtubername+" 유튜버를 편집합니다.\n* 편집할 항목을 선택해주십시오.");
-		System.out.print("1. Youtuber name\n2. Kind of Youtuber\n3. Youtuber Link\n*번호를 입력해주십시오: ");
-		int answer1=input.nextInt();
-		if(answer1==1) {
-			System.out.print("1. Youtuber name항목을 편집합니다\n* Youtuber name: ");
-			String editname=input.next();
-			System.out.println(editname+"(으)로 변경되었습니다.");
-		}
-		else if(answer1==2) {
-			System.out.print("2. Kind of Youtuber항목을 편집합니다\n* Kind of Youtuber: ");
-			String editkind=input.next();
-			System.out.println(editkind+"(으)로 변경되었습니다.");
-		}
-		else if(answer1==3){
-			System.out.print("3. Youtuber Link 항목을 편집합니다\n* Youtuber Link: ");
-			String editlink=input.next();
-			System.out.println(editlink+"(으)로 변경되었습니다.");
-		}
-		else {
-			System.out.println("번호를 잘못 입력하셨습니다.");
-		}
-		case("N"):
-			break;
-		}
 	}
 	public static void viewYoutuber() {
 		Scanner input=new Scanner(System.in);
-		System.out.print("Youtuber name:");
+		System.out.print("Youtuber ID:");
 		String youtubername=input.next();
-		System.out.print("보고자 하는 유튜버가 "+youtubername+"이 맞습니까?\nY or N 입력: ");
-		String answer=input.next();
-		switch(answer){
-		case("Y"):
-			System.out.println(youtubername+" 유튜버의 링크입니다.");
-		System.out.println("https://www.youtube.com/channel/UC99OELa9yvqgkq9ffBvm9iQ");
-		case("N"):
-			break;
-		}
 	}
 }
+
 
 
 
