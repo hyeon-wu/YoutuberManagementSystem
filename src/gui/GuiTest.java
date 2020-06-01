6cde3a3c53eab2421a5df10c05f281460d2c7029
shw16 <shw16@DESKTOP-HV8IUM9>
2020-06-01 15:15:49 +0900

    add GUI

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+	
+	public static void main(String[] args) {
+//		MenuSelection menuselection=new MenuSelection();
+//		YoutuberAdder youtuberadder=new YoutuberAdder();
+		YoutuberViewer youtuberviewer=new YoutuberViewer();
+	}
+
+}
---
+++ src/gui/YoutuberViewer.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class YoutuberViewer extends JFrame{
+
+	public YoutuberViewer() {
+		DefaultTableModel model=new DefaultTableModel();
+		model.addColumn("Id");
+		model.addColumn("Name");
+		model.addColumn("Subscriber num");
+		model.addColumn("Link");
+		
+		
+		JTable table=new JTable(model);
+		JScrollPane sp=new JScrollPane(table);
+		
+		this.add(sp);
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		this.setVisible(true);
+	}
+
+}
